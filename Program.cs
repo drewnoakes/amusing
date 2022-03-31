7e3f4f0be5eccbf9c211ea9e3eef835a5a78ee4c
Drew Noakes <git@drewnoakes.com>
2022-03-31 23:19:58 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
+using Spectre.Console;
+
+var stderr = AnsiConsole.Create(new AnsiConsoleSettings { Out = new AnsiConsoleOutput(Console.Error) });
+
+string? filePath = args switch
+{
+    [string s] => s,
+    _ => null
+};
+
+if (filePath is null)
+{
+    PrintUsage(stderr);
+    return 1;
+}
+
+var instance = MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(i => i.Version).FirstOrDefault();
+
+if (instance is null)
+{
+    stderr.WriteLine("[red]Unable to locate MSBuild. Cannot continue.[/]");
+    return 1;
+}
+
+MSBuildLocator.RegisterInstance(instance);
+
+using var workspace = MSBuildWorkspace.Create();
+
+var errors = new List<string>();
+
+workspace.WorkspaceFailed += (o, e) => errors.Add(e.Diagnostic.Message);
+
+var status = AnsiConsole.Status().Spinner(Spinner.Known.Default);
+
+await status.StartAsync(
+    $"Loading {Path.GetFileName(filePath)}",
+    ctx =>
+    {
+        return filePath.EndsWith(".sln")
+            ? workspace.OpenSolutionAsync(filePath)
+            : workspace.OpenProjectAsync(filePath);
+    });
+
+bool hasWarnings = false;
+
+foreach (var error in errors)
+{
+    stderr.MarkupLineInterpolated($"[purple]{error}[/]");
+    hasWarnings = true;
+}
+
+var collector = new UsingCollector();
+
+await status.StartAsync(
+    "Walking syntax trees",
+    async ctx =>
+    {
+        foreach (var project in workspace.CurrentSolution.Projects)
+        {
+            if (project.Language != "C#")
+            {
+                AnsiConsole.MarkupLineInterpolated($"[yellow]Skipping non-C# project: {project.FilePath}[/]");
+                hasWarnings = true;
+                continue;
+            }
+
+            foreach (var document in project.Documents)
+            {
+                if (document.FilePath?.EndsWith("GlobalUsings.g.cs") == true)
+                {
+                    continue;
+                }
+
+                SyntaxNode? root = await document.GetSyntaxRootAsync();
+
+                collector.Visit(root);
+            }
+        }
+    });
+
+if (hasWarnings)
+{
+    AnsiConsole.WriteLine();
+}
+
+foreach ((string name, int count) in collector.Usings.OrderByDescending(pair => pair.Value).ThenBy(pair => pair.Key))
+{
+    AnsiConsole.MarkupLineInterpolated($"[blue]{count,-6}[/] {name}");
+}
+
+return 0;
+
+static void PrintUsage(IAnsiConsole console)
+{
+    console.MarkupLine(
+        """
+        Usage:
+
+            [green]amusing[/] [blue]<file>[/]
+
+        Where:
+
+            [blue]<file>[/] Path to a .sln or MSBuild project file (such as .csproj)
+        """);
+}
+
+class UsingCollector : CSharpSyntaxWalker
+{
+    public Dictionary<string, int> Usings { get; } = new();
+
+    public override void VisitUsingDirective(UsingDirectiveSyntax node)
+    {
+        string name = node.Name.ToString();
+
+        if (!Usings.TryGetValue(name, out int count))
+        {
+            count = 0;
+        }
+
+        count++;
+        Usings[name] = count;
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+﻿# Amusing
+
+A command-line tool that shows what namespaces you am-using.
+
+Intended to help make the best use of the [implicit usings](https://aka.ms/csharp-implicit-usings) features of C# 10 / .NET 6.
+
+## Example
+
+```bash
+$ amusing MySolution.sln
+225    Xunit
+207    Moq
+151    BigCorp.Splines.Reticulator
+<snip>
+```
+
+This shows that both `Xunit` and `Moq` namespaces are widely used.
+
+Assuming C# 10 or later and .NET SDK 6 or later, we can make those explicit by placing a `Directory.Build.props` file in our `test/` path, containing:
+
+```xml
+<Project>
+
+  <ItemGroup>
+    <Using Include="Xunit" />
+    <Using Include="Moq" />
+  </ItemGroup>
+
+</Project>
+```
+
+With that, all source under that location will implicitly import those namespaces.
+
+You can run a Roslyn codefix to remove the previous `using Xunit;` and `using Moq;` directives, which will now be marked as redundant.
+
+## Installation
+
+This utility is provided as a .NET global tool.
+
+```bash
+dotnet tool install -g amusing
+```
+
+## Thanks
+
+- [Spectre.Console](https://github.com/spectreconsole/spectre.console) for making the console output more fun.
